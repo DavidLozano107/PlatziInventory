e4e5b4ce5da62ad7884cf19861a0a68b6d23fda6
David Lozano <Davidlozano107@gmail.com>
2021-07-20 14:58:19 -0500

    Agrega CRUD a las entidades faltantes

--- Business/B_Storage.cs
+++ Business/B_Storage.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using DataAccess;
+using Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business
 {
 	public class B_Storage
 	{
-		prub
+		public List<StorageEntity> GetListStorages()
+		{
+			using var db = new InventaryContext();
+			return db.Storages.ToList();
+		}
+
+		public void CreateStorage(StorageEntity storage)
+		{
+			using var db = new InventaryContext();
+			db.Storages.Add(storage);
+			db.SaveChanges();
+		}
+
+		public void UpdateStorage(StorageEntity storage)
+		{
+			using var db = new InventaryContext();
+			db.Storages.Update(storage);
+			db.SaveChanges();
+		}
 
 	}
 }
--- Business/B_Warehouse.cs
+++ Business/B_Warehouse.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using DataAccess;
+using Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business
 {
 	public class B_Warehouse
 	{
+		public List<WarehouseEntity> GetListWarehouse()
+		{
+			using var db = new InventaryContext();
+			return db.Warehouses.ToList();
+		}
+
+		public void CreateWarehouse(WarehouseEntity warehouse)
+		{
+			using var db = new InventaryContext();
+			db.Warehouses.Add(warehouse);
+			db.SaveChanges();
+		}
+
+		public void UpdateWarehouse(WarehouseEntity warehouse)
+		{
+			using var db = new InventaryContext();
+			db.Warehouses.Update(warehouse);
+			db.SaveChanges();
+		}
+
 	}
 }
